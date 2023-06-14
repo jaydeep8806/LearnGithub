7647e325d7e0106681b112c1e0ed4453a0cc0816
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2023-06-14 23:40:56 +0530

    learn if/else

--- Hello.c 14 - 06 - 23.c
+++ Hello.c 14 - 06 - 23.c
@@ -3,16 +3,17 @@
 int main() {
     int age;
 
-    printf("Enter a age: ");
+    printf("Enter a age : ");
     scanf("%d", &age);
 
     if (age >= 18) {
-        printf("adult \n");
-
-    } else if (age > 13 && < 18) {
-        printf("teenager \n");
+        printf("adult\n");
+    } 
+    else if (age > 13 && age < 18) {
+        printf("teeneger \n");
     } else {
         printf("child\n");
     }
+
     return 0;
 }
