87a50002221812dc2b4370a704da50775f54b80e
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2023-06-19 23:22:41 +0530

    learn for loops, incerasing and decreasing no., infinite loop

---
+++ hiii_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+
+   int i = 1 ;
+   
+   printf("%d \n" , i++); //   first i value and +1 increse 
+                          //   ++i ena mate +1 increse and i value  
+   printf("%d \n" , i);
+    
+    return 0;
+}
---
+++ hiii_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main () {
+
+   for  (float i = 0.5 ; i<= 9.5 ; i++) {
+    printf("%f \n", i );
+ } 
+
+for (char ch = 'a'; ch<='z'; ch++){
+    printf("%c \n" , ch);
+}
+
+   return 0;
+}
