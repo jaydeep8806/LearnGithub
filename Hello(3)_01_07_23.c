23ead732f9f8485e94ee4da5be4c29a852742d62
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2023-07-02 21:00:06 +0530

    learn array with question

---
+++ Hello(3)_01_07_23.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main () {
+    //2 * 3
+
+    int marks [2][3]; // _ _ _ | _ _ _ 
+    marks [0][0] = 92;
+    marks [0][1] = 94;
+    marks [0][2] = 96;
+
+    marks [0][0] = 92;
+    marks [1][1] = 94;
+    marks [2][2] = 96;
+
+
+    return 0;
+}
